265590bd206c5a43aea1f45becc98ecab271c0a3
Anjesh Tuladhar <anjesh@yipl.com.np>
2018-01-16 15:09:03 +0545

    scraping on 2018.01.16

---
+++ organisation.xml/iucn_nl-2016q1org/20160430-Q1-iucn_admin-org.php
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!--Final file location will be subject to change-->
+<iati-organisations version="2.02" generated-datetime="2016-04-29T16:06:38+00:00">
+  <iati-organisation last-updated-datetime="2016-04-29T16:06:38+00:00" xml:lang="en" default-currency="EUR">
+    <organisation-identifier>NL-KVK-41180885</organisation-identifier>
+    <name>
+      <narrative xml:lang="en">IUCN National Committee of The Netherlands</narrative>
+      <narrative xml:lang="nl">Nederlands Comite IUCN</narrative>
+    </name>
+    <reporting-org type="22" ref="NL-KVK-41180885">
+      <narrative xml:lang="nl">IUCN NL</narrative>
+    </reporting-org>
+    <total-budget status="2">
+      <period-start iso-date="2016-01-01"/>
+      <period-end iso-date="2016-12-31"/>
+      <value currency="EUR" value-date="2016-04-29">11748054</value>
+    </total-budget>
+    <total-budget status="2">
+      <period-start iso-date="2016-01-01"/>
+      <period-end iso-date="2016-12-31"/>
+      <value currency="EUR" value-date="2016-04-29">2600000</value>
+    </total-budget>
+    <total-budget status="2">
+      <period-start iso-date="2017-01-01"/>
+      <period-end iso-date="2017-12-31"/>
+      <value value-date="2016-04-29">11819842</value>
+    </total-budget>
+    <total-budget status="2">
+      <period-start iso-date="2017-01-01"/>
+      <period-end iso-date="2017-12-31"/>
+      <value value-date="2016-04-29">2600000</value>
+    </total-budget>
+    <total-budget status="2">
+      <period-start iso-date="2018-01-01"/>
+      <period-end iso-date="2018-12-31"/>
+      <value value-date="2016-05-29">11916190</value>
+    </total-budget>
+    <total-budget status="2">
+      <period-start iso-date="2017-12-01"/>
+      <period-end iso-date="2018-12-31"/>
+      <value value-date="2016-04-29">2600000</value>
+    </total-budget>
+    <total-budget status="2">
+      <period-start iso-date="2019-01-01"/>
+      <period-end iso-date="2019-12-31"/>
+      <value value-date="2016-04-29">11969509</value>
+    </total-budget>
+    <total-budget status="2">
+      <period-start iso-date="2019-01-01"/>
+      <period-end iso-date="2019-12-31"/>
+      <value value-date="2016-04-29">2600000</value>
+    </total-budget>
+    <total-budget status="2">
+      <period-start iso-date="2020-01-01"/>
+      <period-end iso-date="2020-12-31"/>
+      <value value-date="2016-04-29">12070155</value>
+    </total-budget>
+    <total-budget status="2">
+      <period-start iso-date="2020-01-01"/>
+      <period-end iso-date="2020-12-31"/>
+      <value value-date="2016-04-29">2600000</value>
+    </total-budget>
+    <document-link format="application/vnd.ms-excel" url="http://cmsdata.iucn.org/downloads/budget_srjs_and_gla_2016tm2020.pdf">
+      <title>
+        <narrative xml:lang="en">Budget document SRJS and GLA 2016-2020</narrative>
+      </title>
+      <category code="A05"/>
+      <language code="en"/>
+      <document-date iso-date="2016-04-01"/>
+      <recipient-country code="NL">
+        <narrative xml:lang="en">-</narrative>
+      </recipient-country>
+    </document-link>
+  </iati-organisation>
+</iati-organisations>
